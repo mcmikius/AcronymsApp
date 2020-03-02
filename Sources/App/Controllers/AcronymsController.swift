590a6051cc7b2517424851944cfc34ba734defaf
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-03-02 16:47:53 +0200

    add relationship via User and Acronym

--- Sources/App/Controllers/AcronymsController.swift
+++ Sources/App/Controllers/AcronymsController.swift
@@ -22,6 +22,7 @@ struct AcronymsController: RouteCollection {
         acronymsRoutes.get("search", use: searchHandler)
         acronymsRoutes.get("first", use: getFirstHandler)
         acronymsRoutes.get("sorted", use: sortedHandler)
+        acronymsRoutes.get(Acronym.parameter, "user", use: getUserHandler)
     }
     
     func getAllHandler(_ req: Request) throws -> Future<[Acronym]> {
@@ -40,6 +41,7 @@ struct AcronymsController: RouteCollection {
         return try flatMap(to: Acronym.self, req.parameters.next(Acronym.self), req.content.decode(Acronym.self)) { acronym, updatedAcronym in
             acronym.short = updatedAcronym.short
             acronym.long = updatedAcronym.long
+            acronym.userID = updatedAcronym.userID
             return acronym.save(on: req)
         }
     }
@@ -64,5 +66,11 @@ struct AcronymsController: RouteCollection {
     
     func sortedHandler(_ req: Request) throws -> Future<[Acronym]> {
         return Acronym.query(on: req).sort(\.short, .ascending).all()
+    }
+    
+    func getUserHandler(_ req: Request) throws -> Future<User> {
+        return try req.parameters.next(Acronym.self).flatMap(to: User.self) { acronym in
+            acronym.user.get(on: req)
+        }
     }
 }
---
+++ Sources/App/Models/User.swift
@@ -0,0 +1,31 @@
+//
+//  User.swift
+//  AcronymsAppPackageDescription
+//
+//  Created by Mykhailo Bondarenko on 02.03.2020.
+//
+
+import Foundation
+import Vapor
+import FluentPostgreSQL
+
+final class User: Codable {
+    var id: UUID?
+    var name: String
+    var username: String
+    
+    init(name: String, username: String) {
+        self.name = name
+        self.username = username
+    }
+}
+
+extension User: PostgreSQLUUIDModel {}
+extension User: Content {}
+extension User: Migration {}
+extension User: Parameter {}
+extension User {
+    var acronyms: Children<User, Acronym> {
+        return children(\.userID)
+    }
+}
