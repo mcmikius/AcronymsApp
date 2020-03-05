ad406cb0a578b9ea74fd70f57a5f5f83b0a83fc0
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-03-05 09:46:25 +0200

    Add password Username unique Public class for user

---
+++ Sources/App/Models/User/User+Migration.swift
@@ -0,0 +1,19 @@
+//
+//  User+Migration.swift
+//  AcronymsAppPackageDescription
+//
+//  Created by Mykhailo Bondarenko on 05.03.2020.
+//
+
+import Foundation
+import Vapor
+import FluentPostgreSQL
+
+extension User: Migration {
+    static func prepare(on connection: PostgreSQLConnection) -> Future<Void> {
+        return Database.create(self, on: connection) { builder in
+            try addProperties(to: builder)
+            builder.unique(on: \.username)
+        }
+    }
+}
---
+++ Sources/App/Models/User/User.swift
@@ -0,0 +1,57 @@
+//
+//  User.swift
+//  AcronymsAppPackageDescription
+//
+//  Created by Mykhailo Bondarenko on 02.03.2020.
+//
+
+import Foundation
+import Vapor
+import FluentPostgreSQL
+
+final class User: Codable {
+
+    var id: UUID?
+    var name: String
+    var username: String
+    var password: String
+    
+    init(name: String, username: String, password: String) {
+        self.name = name
+        self.username = username
+        self.password = password
+    }
+
+    final class Public: Codable {
+        var id: UUID?
+        var name: String
+        var username: String
+
+        init(id: UUID?, name: String, username: String) {
+            self.id = id
+            self.name = name
+            self.username = username
+        }
+    }
+}
+
+extension User {
+
+    var acronyms: Children<User, Acronym> {
+        return children(\.userID)
+    }
+
+    func convertToPublic() -> User.Public {
+        return User.Public(id: id, name: name, username: username)
+    }
+}
+
+extension User.Public: Content {}
+
+extension Future where T: User {
+    func convertToPublic() -> Future<User.Public> {
+        return self.map(to: User.Public.self) { user in
+            return user.convertToPublic()
+        }
+    }
+}
