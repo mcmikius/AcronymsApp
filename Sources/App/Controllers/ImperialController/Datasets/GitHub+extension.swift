fd27edce844b6c2422f07d442d0c1e0334388ace
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-03-06 12:51:03 +0200

    add email to git Hub

--- Sources/App/Controllers/ImperialController/Datasets/GitHub+extension.swift
+++ Sources/App/Controllers/ImperialController/Datasets/GitHub+extension.swift
@@ -23,4 +23,22 @@ extension GitHub {
             return try response.content.syncDecode(GitHubUserInfo.self)
         }
     }
+
+    static func getEmails(on request: Request) throws -> Future<[GitHubEmailInfo]> {
+        var headers = HTTPHeaders()
+        headers.bearerAuthorization =
+            try BearerAuthorization(token: request«.accessToken())
+
+        let githubUserAPIURL = "https://api.github.com/user/emails"
+        return try request.client().get(githubUserAPIURL, headers: headers).map(to: [GitHubEmailInfo].self) { response in
+            guard response.http.status == .ok else {
+                if response.http.status == .unauthorized {
+                    throw Abort.redirect(to: "/login-github")
+                } else {
+                    throw Abort(.internalServerError)
+                }
+            }
+            return try response.content.syncDecode([GitHubEmailInfo].self)
+        }
+    }
 }
--- Sources/App/Controllers/ImperialController/ImperialController.swift
+++ Sources/App/Controllers/ImperialController/ImperialController.swift
@@ -18,14 +18,14 @@ struct ImperialController: RouteCollection {
         guard let githubCallbackURL = Environment.get("GITHUB_CALLBACK_URL") else {
             fatalError("GitHub callback URL not set")
         }
-        try router.oAuth(from: GitHub.self, authenticate: "login-github", callback: githubCallbackURL, completion: processGitHubLogin)
+        try router.oAuth(from: GitHub.self, authenticate: "login-github", callback: githubCallbackURL, scope: ["user:email"], completion: processGitHubLogin)
     }
 
     func processGoogleLogin(request: Request, token: String) throws -> Future<ResponseEncodable> {
         return try Google.getUser(on: request).flatMap(to: ResponseEncodable.self) { userInfo in
             return User.query(on: request).filter(\.username == userInfo.email).first().flatMap(to: ResponseEncodable.self) { foundUser in
                 guard let existingUser = foundUser else {
-                    let user = User(name: userInfo.name, username: userInfo.email, password: UUID().uuidString)
+                    let user = User(name: userInfo.name, username: userInfo.email, password: UUID().uuidString, email: userInfo.email)
                     return user.save(on: request).map(to: ResponseEncodable.self) { user in
                         try request.authenticateSession(user)
                         return request.redirect(to: "/")
@@ -38,12 +38,10 @@ struct ImperialController: RouteCollection {
     }
 
     func processGitHubLogin(request: Request, token: String) throws -> Future<ResponseEncodable> {
-        return try GitHub.getUser(on: request).flatMap(to: ResponseEncodable.self) { userInfo in
+        return try flatMap(to: ResponseEncodable.self, GitHub.getUser(on: request), GitHub.getEmails(on: request)) { userInfo, emailInfo in
             return User.query(on: request).filter(\.username == userInfo.login).first().flatMap(to: ResponseEncodable.self) { foundUser in
                 guard let existingUser = foundUser else {
-                    let user = User(name: userInfo.name,
-                        username: userInfo.login,
-                        password: UUID().uuidString)
+                    let user = User(name: userInfo.name, username: userInfo.login, password: UUID().uuidString, email: emailInfo[0].email)
                     return user.save(on: request).map(to: ResponseEncodable.self) { user in
                         try request.authenticateSession(user)
                         return request.redirect(to: "/")
