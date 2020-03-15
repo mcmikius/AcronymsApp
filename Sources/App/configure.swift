bc33aaa55c65d77c00499573f0e5ab92a64558eb
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-03-15 14:01:47 +0200

    configured heroku database

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -29,24 +29,46 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
 
     // Register the configured PostgreSQL database to the database config.
     var databases = DatabasesConfig()
-    let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
-    let username = Environment.get("DATABASE_USER") ?? "vapor"
-    let password = Environment.get("DATABASE_PASSWORD") ?? "password"
-    let databaseName: String
-    let databasePort: Int
-    if (env == .testing) {
+//    let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
+//    let username = Environment.get("DATABASE_USER") ?? "vapor"
+//    let password = Environment.get("DATABASE_PASSWORD") ?? "password"
+//    let databaseName: String
+//    let databasePort: Int
+//    if (env == .testing) {
+//        databaseName = "vapor-test"
+//        if let testPort = Environment.get("DATABASE_PORT") {
+//            databasePort = Int(testPort) ?? 5433
+//        } else {
+//            databasePort = 5433
+//        }
+//    } else {
+//        databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+//        databasePort = 5432
+//    }
+//
+//    let databaseConfig = PostgreSQLDatabaseConfig(hostname: hostname, port: databasePort, username: username, database: databaseName, password: password)
+    let databaseConfig: PostgreSQLDatabaseConfig
+    if let url = Environment.get("DATABASE_URL") {
+      databaseConfig = PostgreSQLDatabaseConfig(url: url)!
+    } else {
+      let hostname =
+        Environment.get("DATABASE_HOSTNAME") ?? "localhost"
+      let databaseName: String
+      let databasePort: Int
+      if env == .testing {
         databaseName = "vapor-test"
         if let testPort = Environment.get("DATABASE_PORT") {
-            databasePort = Int(testPort) ?? 5433
+          databasePort = Int(testPort) ?? 5433
         } else {
-            databasePort = 5433
+          databasePort = 5433
         }
-    } else {
-        databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+      } else {
+        databaseName = "vapor"
         databasePort = 5432
+      }
+        
+      databaseConfig = PostgreSQLDatabaseConfig(hostname: hostname, port: databasePort, username: "vapor", database: databaseName, password: "password")
     }
-
-    let databaseConfig = PostgreSQLDatabaseConfig(hostname: hostname, port: databasePort, username: username, database: databaseName, password: password)
     let database = PostgreSQLDatabase(config: databaseConfig)
     databases.add(database: database, as: .psql)
     services.register(databases)
